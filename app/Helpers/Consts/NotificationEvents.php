acc90bcc4c1f0557ac21055181f7e3071034cafd
Michael Mathews <michaelmathewssar@gmail.com>
2021-04-21 08:11:09 -0500

    Create migration for notifiactions && create constants for events

---
+++ app/Helpers/Consts/NotificationEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Helpers\Consts;
+
+
+final class NotificationEvents
+{
+    const PASSWORD_CHANGED = 'Password changed';
+    const EMAIL_CHANGED = 'Email changed';
+}
---
+++ app/Models/Notification.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = ['user_id', 'event', 'read'];
+
+    public static function boot ()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->setCreatedAt($model->freshTimestamp());
+        });
+    }
+
+}
