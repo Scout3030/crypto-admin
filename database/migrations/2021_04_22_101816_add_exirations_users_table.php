58c6e3c8a8b3c53a7bce247d8f81c5bfc9bbde14
bpartner <azinchenko@bpartner.com.ua>
2021-04-22 14:12:43 +0300

    CMA-76 Password expiration reaminder

---
+++ database/migrations/2021_04_22_101816_add_exirations_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExirationsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dateTime('exp_date')->useCurrent();
+            $table->json('properties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('exp_date');
+            $table->dropColumn('properties');
+        });
+    }
+}
