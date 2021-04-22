3bc3f784de55b525716020938461ac6b8fccad12
Michael Mathews <michaelmathewssar@gmail.com>
2021-04-22 15:58:48 -0500

    refactor | style for buttons on merchant list

--- resources/views/user/merchant-action.blade.php
+++ resources/views/user/merchant-action.blade.php
@@ -1,4 +1,6 @@
+<div class="d-flex">
 @can('merchant-users-actions-button')
-    <a href="{{route('client.edit', $id)}}">Edit</a>
-    <button class="delete-permission" data-id="{{$id}}" data-name="{{$first_name}}">Delete</button>
+    <a href="{{route('user.edit', $id)}}" class="btn btn-primary shadow btn-xs sharp"><i class="fa fa-pencil"></i></a>
+    <button class="delete-permission btn btn-danger shadow btn-xs sharp" data-id="{{$id}}" data-name="{{$first_name}}"><i class="fa fa-trash"></i></button>
 @endcan
+</div>
