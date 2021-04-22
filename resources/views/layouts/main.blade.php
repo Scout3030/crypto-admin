eedc31244dee04ac0b4f9702bffbe540e573dff2
zorobabel <zorobabel.omicron@gmail.com>
2021-04-21 21:49:00 -0500

    Files implement livewire on user form process complete

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="shortcut icon" type="image/png" href="{{ asset('img/favicon.png') }}"/>
 
+    @livewireStyles
+
 	<!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!-- FontAwesome -->
@@ -15,8 +17,6 @@
 	<!-- Styles -->
 	<link rel="stylesheet" href="{{ asset('fonts/flaticon.css') }}">
 	<link rel="stylesheet" href="{{ mix('css/main.css') }}">
-
-    @livewireStyles
 
     @stack('css')
 
